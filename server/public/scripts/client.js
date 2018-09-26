e3ead39b429a1e80c488608992489c4ee5ffeb4c
brdrummer <brdrummer@hotmail.com>
2018-09-26 16:03:42 -0500

    Client Routes Created, SQL tables Created, Client Controllers Created and Sourced

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,1 +1,28 @@
-console.log('client sourced');
+const petHotelApp = angular.module('PetHotelApp', ['ngRoute']);
+// ROUTES
+petHotelApp.config(['$routeProvider', function ($routeProvider) {
+    $routeProvider.when('/', {
+        template: '<h1>Home</h1>'
+    }).when('/pets', {
+        templateUrl: '../views/pets.html',
+        controller: 'PetsController as vm'
+    }).when('/owners', {
+        templateUrl: '../views/owners.html',
+        controller: 'OwnersController as vm'
+    })
+}]); //END ROUTES 
+
+// PETS CONTROLLER
+petHotelApp.controller('PetsController', ['$http', function ($http) {
+    let vm = this;
+
+
+
+}])
+// OWNERS CONTROLLER
+petHotelApp.controller('OwnersController', ['$http', function ($http) {
+    let vm = this;
+
+
+
+}])
